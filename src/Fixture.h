cc3c9af13d9837e2f761c827d6fde263884d0240
Q <andrea@pushthemonkey.com>
2013-07-31 14:50:51 +0100

    remove nocte namespace

--- src/Fixture.h
+++ src/Fixture.h
@@ -16,94 +16,90 @@
 #include "cinder/gl/Vbo.h"
 #include "cinder/ObjLoader.h"
 
+class Fixture;
+typedef std::shared_ptr<Fixture> FixtureRef;
 
-namespace nocte {
+class Fixture {
     
-    class Fixture;
-    typedef std::shared_ptr<Fixture> FixtureRef;
+    friend class Scene;
+    friend class Stage;
     
-    class Fixture {
+    
+public: 
+    
+    static FixtureRef create( ci::Vec3f pos, int dmxChannel, ci::gl::VboMesh *mesh = NULL )
+    {
+        return FixtureRef( new Fixture( pos, dmxChannel, mesh ) );
+    }
+    
+    ~Fixture() {}
+    
+    virtual void update( float val, float fadeInSpeed, float fadeOutSpeed )
+    {                
+        mTargetValue = ci::math<float>::clamp(val, 0.0f, 1.0f);
         
-        friend class Scene;
-        friend class Stage;
+        if ( mValue < mTargetValue ) 
+            mValue = std::min(mValue + fadeInSpeed, mTargetValue);
+        else 
+            mValue = std::max(mValue - fadeOutSpeed , mTargetValue);
         
+  //      mBrightness = math<float>::clamp( mValue * gLiveMasterBrightness + gLiveBaseBrightness, 0.0f, 1.0f );
+        //	console() << "fixture " << mBrightness << " " <<  mValue << " " << gLiveMasterBrightness << " " << gLiveBaseBrightness << endl;
+    }
+    
+    virtual void render() 
+    {
+        if ( !mMesh )
+            return;
         
-    public: 
-        
-        static FixtureRef create( ci::Vec3f pos, int dmxChannel, ci::gl::VboMesh *mesh = NULL )
-        {
-            return FixtureRef( new Fixture( pos, dmxChannel, mesh ) );
-        }
-        
-        ~Fixture() {}
-        
-        virtual void update( float val, float fadeInSpeed, float fadeOutSpeed )
-        {                
-            mTargetValue = ci::math<float>::clamp(val, 0.0f, 1.0f);
-            
-            if ( mValue < mTargetValue ) 
-                mValue = std::min(mValue + fadeInSpeed, mTargetValue);
-            else 
-                mValue = std::max(mValue - fadeOutSpeed , mTargetValue);
-            
-      //      mBrightness = math<float>::clamp( mValue * gLiveMasterBrightness + gLiveBaseBrightness, 0.0f, 1.0f );
-            //	console() << "fixture " << mBrightness << " " <<  mValue << " " << gLiveMasterBrightness << " " << gLiveBaseBrightness << endl;
-        }
-        
-        virtual void render() 
-        {
-            if ( !mMesh )
-                return;
-            
-            ci::gl::color( ci::ColorA( 0.9f, 0.21f, 0.34f, mValue ) );
-            ci::gl::pushMatrices();
-            ci::gl::translate(mPos);
-            ci::gl::draw( *mMesh );
-            ci::gl::popMatrices();
-        }
-        
-        ci::Vec3f   getPos() { return mPos; }
-        
-        ci::Vec2f	getPos2f() { return mPos.xz(); }
-        
-        float getValue() { return mValue; }
-        
-        float getTargetValue() { return mTargetValue; }
-        
-        int getChannel() { return mDMXChannel; }
-          
-        void setMesh( ci::gl::VboMesh *mesh ) { mMesh = mesh; }
-        
-        void loadMesh( const std::string &meshRes )
-        {
-            ci::ObjLoader loader( (ci::DataSourceRef)ci::app::loadResource(meshRes) );
-            ci::TriMesh	mesh; 
-            loader.load( &mesh );
-            mMesh = new ci::gl::VboMesh( mesh );
-        }
-        
+        ci::gl::color( ci::ColorA( 0.9f, 0.21f, 0.34f, mValue ) );
+        ci::gl::pushMatrices();
+        ci::gl::translate(mPos);
+        ci::gl::draw( *mMesh );
+        ci::gl::popMatrices();
+    }
+    
+    ci::Vec3f   getPos() { return mPos; }
+    
+    ci::Vec2f	getPos2f() { return mPos.xz(); }
+    
+    float getValue() { return mValue; }
+    
+    float getTargetValue() { return mTargetValue; }
+    
+    int getChannel() { return mDMXChannel; }
+      
+    void setMesh( ci::gl::VboMesh *mesh ) { mMesh = mesh; }
+    
+    void loadMesh( const std::string &meshRes )
+    {
+        ci::ObjLoader loader( (ci::DataSourceRef)ci::app::loadResource(meshRes) );
+        ci::TriMesh	mesh; 
+        loader.load( &mesh );
+        mMesh = new ci::gl::VboMesh( mesh );
+    }
+    
 
-    protected:
+protected:
 
-        Fixture( ci::Vec3f pos, int dmxChannel, ci::gl::VboMesh *mesh = NULL )
-        : mPos(pos), mDMXChannel(dmxChannel), mValue(0.0f), mTargetValue(0.0f), mMesh(mesh) {}
+    Fixture( ci::Vec3f pos, int dmxChannel, ci::gl::VboMesh *mesh = NULL )
+    : mPos(pos), mDMXChannel(dmxChannel), mValue(0.0f), mTargetValue(0.0f), mMesh(mesh) {}
 
-        
-    protected:
-        
-        ci::Vec3f           mPos;
-        int                 mDMXChannel;
-        float               mValue;             // val normalised
-        float               mTargetValue;
-        ci::gl::VboMesh     *mMesh;
-        
-    private:
-        // disallow
-        Fixture(const Fixture&);
-        Fixture& operator=(const Fixture&);
     
-    };
+protected:
     
-}
+    ci::Vec3f           mPos;
+    int                 mDMXChannel;
+    float               mValue;             // val normalised
+    float               mTargetValue;
+    ci::gl::VboMesh     *mMesh;
+    
+private:
+    // disallow
+    Fixture(const Fixture&);
+    Fixture& operator=(const Fixture&);
+
+};
+
 
 #endif
